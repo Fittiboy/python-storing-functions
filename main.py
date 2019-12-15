d355ee45509adaa6a6eb2ac47a6abe0475d2f5f7
Fitti <28876473+Fittiboy@users.noreply.github.com>
2019-12-15 01:27:00 +0100

    Add main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+from store_functions import store, remove
+
+def main():
+    name = input("What is the name of the function you want to store/remove? ")
+    choice = input('Do you want to (s)tore or (r)emove a function? (s/r) ')
+    valid_choices = ["s", "r"]
+    while choice not in valid_choices:
+        choice = input('Please select either "s" for store or "r" for remove: ')
+    if choice == "s":
+        store(name)
+    elif choice == "r":
+        remove(name)
+
+if __name__ == "__main__":
+    main()
